0d754b4abf7a948dcea55574755d3dca5abba841
Max van der Ree <maxvdree@hotmail.com>
2022-02-21 20:19:05 +0100

    Move data + add bg snapshots (temporary)

--- VoxelReconstruction/src/main.cpp
+++ VoxelReconstruction/src/main.cpp
@@ -9,6 +9,7 @@ using namespace nl_uu_science_gmt;
 int main(
 		int argc, char** argv)
 {
+
 	VoxelReconstruction::showKeys();
 	VoxelReconstruction vr("data" + std::string(PATH_SEP), 4);
 	vr.run(argc, argv);
